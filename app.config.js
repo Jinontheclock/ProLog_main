806b936729632bfa4545f4a8c31707b7180e36da
Leana <leanale003@gmail.com>
2025-11-23 20:55:38 -0800

    Removed sensitive API key from app

--- app.config.js
+++ app.config.js
@@ -4,7 +4,6 @@ export default {
     slug: "prolog",
     version: "1.0.0",
     extra: {
-      API_KEY: "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"
-    },
+      API_KEY: "removed for security",
   },
 };
